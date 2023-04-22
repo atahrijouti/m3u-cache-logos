95279bcc8f5f0efb07a1b95768852b14c26d6ddc
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-04-22 17:28:20 +0200

    split things up a bit

--- src/cache.ts
+++ src/cache.ts
@@ -1,34 +1,37 @@
 import { promises as fs } from "fs"
-import { M3uParser } from "m3u-parser-generator"
+import { M3uParser, M3uPlaylist } from "m3u-parser-generator"
 import { M3uMedia } from "m3u-parser-generator/src/m3u-playlist"
 
 const sanitizeFileName = (text: string) => {
-    let t = text
+  let t = text
     .replace(/[^a-z0-9]/gi, "_")
     .replace(/_+/gi, "_")
     .toLowerCase()
-  if(t[0] === "_") {
+  if (t[0] === "_") {
     t = t.substring(1)
   }
-  if(t[t.length - 1] === "_") {
+  if (t[t.length - 1] === "_") {
     t = t.substring(0, t.length - 1)
   }
   return t
 }
-const program = async () => {
+
+const parseM3UPlaylist = async () => {
   const playlistRaw = await fs.readFile("input/playlist.m3u", {
     encoding: "utf-8",
   })
-  const playlist = M3uParser.parse(playlistRaw)
-  const dict = playlist.medias.reduce<Record<string, string>>(
-    (acc, channel: M3uMedia) => {
-      const name = sanitizeFileName(channel.attributes["tvg-name"] ?? "")
-      acc[name] = channel.attributes["tvg-logo"] ?? ""
-      return acc
-    },
-    {}
-  )
-  console.log(dict)
+
+  return M3uParser.parse(playlistRaw)
 }
+
+const buildLogoDict = (playlist: M3uPlaylist) => {
+  const dict = playlist.medias.reduce<Record<string, string>>((acc, channel: M3uMedia) => {
+    const name = sanitizeFileName(channel.attributes["tvg-name"] ?? "")
+    acc[name] = channel.attributes["tvg-logo"] ?? ""
+    return acc
+  }, {})
+}
+
+const program = async () => {}
 
 program().catch(console.error)
